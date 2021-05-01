273839687f4835dd79dfc6bd20a5ac782cf5e2f6
NteFlix <83513417+NteFlix@users.noreply.github.com>
2021-05-01 23:29:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,14 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBz1w3w38lUoay_wBEFnw5-BE8xATw8I64",
+  authDomain: "nite-e5862.firebaseapp.com",
+  databaseURL: "https://nite-e5862-default-rtdb.firebaseio.com",
+  projectId: "nite-e5862",
+  storageBucket: "nite-e5862.appspot.com",
+  messagingSenderId: "608818230167",
+  appId: "1:608818230167:web:de3783b9f7e0fc7d748ad2"
 };
 
      
